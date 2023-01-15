633ba82d35e813b7f5eefa70eb057d457e0cd851
CharlesXu1124 <xuzheyuan961124@gmail.com>
2023-01-15 03:18:00 -0800

    added extra command for motor

--- src/esp32_serial_node.cpp
+++ src/esp32_serial_node.cpp
@@ -5,8 +5,6 @@
 
 using std::placeholders::_1;
 
-
-
 class RobotControlNode : public rclcpp::Node
 {
 public:
@@ -16,32 +14,45 @@ public:
     cmd_subscriber = this->create_subscription<std_msgs::msg::UInt32>(
         "/robot_cmd", 10, std::bind(&RobotControlNode::cmd_received, this, _1));
 
-    serial = std::make_shared<SimpleSerial>("/dev/ttyUSB0", 115200);
-    // try
-    // {
+    serial = std::make_shared<SimpleSerial>("/dev/ttyUSB2", 9600);
+    try
+    {
 
-    //   
-    //   serial->writeString("f");
-    // }
-    // catch (boost::system::system_error &e)
-    // {
-    //   std::cout << "Error: " << e.what() << std::endl;
-    // }
+      serial->writeString(" ");
+    }
+    catch (boost::system::system_error &e)
+    {
+      std::cout << "Error: " << e.what() << std::endl;
+    }
   }
 
 private:
   void cmd_received(const std_msgs::msg::UInt32::SharedPtr cmd) const
   {
+    if (cmd->data == 0)
+    {
+      serial->writeString("w");
+    }
+
     if (cmd->data == 1)
     {
-      serial->writeString("f");
+      serial->writeString("s");
     }
 
     if (cmd->data == 2)
     {
-      serial->writeString("c");
+      serial->writeString("a");
     }
-    
+
+    if (cmd->data == 3)
+    {
+      serial->writeString("d");
+    }
+
+    if (cmd->data == 4)
+    {
+      serial->writeString(" ");
+    }
   }
 
   rclcpp::Subscription<std_msgs::msg::UInt32>::SharedPtr cmd_subscriber;
